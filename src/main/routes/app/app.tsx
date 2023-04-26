7841abd77b6a0d834a5c1701a2076bc5f993d3c8
Dominik <domimik.kl@wp.pl>
2023-04-26 15:04:20 +0200

    alse I dont know wwhere I am bu I have to move to linux

--- src/main/routes/app/app.tsx
+++ src/main/routes/app/app.tsx
@@ -1,12 +1,26 @@
 import React, { useState, useRef, useEffect, useMemo } from "react";
-import { Canvas, useFrame } from "@react-three/fiber";
-import { OrbitControls, Scroll, ScrollControls, SoftShadows } from "@react-three/drei";
+import { Canvas, useFrame, useThree } from "@react-three/fiber";
+import { OrbitControls, Scroll, ScrollControls, SoftShadows, Html } from "@react-three/drei";
 import { Group, Object3D, SpotLight as test } from "three";
 import { Stats } from "@react-three/drei";
 
 import SchoolClass from "./banner/assests/Threejs/schoolClass";
 import Cube from "./banner/assests/Threejs/cube";
 import Banner from "./banner/banner";
+
+const HtmlTest = () => {
+
+  const { gl } = useThree();
+
+  return(
+    <Html transform portal={{ current: gl.domElement.parentNode }} >
+      <div className="bg-white">
+        rest in test
+      </div>
+    </Html>
+  )
+}
+
 
 const App = () => {
 
@@ -26,13 +40,16 @@ const App = () => {
   return (
     <div className='bg w-screen h-screen'>
       <Canvas camera={{ position: [0, 5, 12] }} shadows>
-        <ambientLight intensity={0.01} />
+        <ambientLight intensity={0.11} />
 
         {/* <Cube sliders={sliders}/> */}
 
-        <ScrollControls pages={80} damping={0.15}>
+
+        <ScrollControls pages={8} damping={0.15}>
           <SchoolClass />
 
+
+          <HtmlTest />
           {/* <Cube sliders={sliders} /> */}
           <Scroll html>
             <Banner />
